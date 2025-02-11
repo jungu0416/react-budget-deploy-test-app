a9b04ccd97e0cb880508468bb0c51bfbd38fd39d
jungu0416 <jungu0416@naver.com>
2025-02-11 22:25:24 +0900

    플젝 생성

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './ExpenseItem.css'
+import { MdEdit } from 'react-icons/md'
+import { MdDelete } from 'react-icons/md'
 
-export class ExpenseItem extends Component {
-  render() {
-    return (
-      <li className="item">
-        <div className="info">
-          <span className="expense">학원등록</span>
-          <span className="amount">1000원</span>
-        </div>
-        <div>
-          <button className="edit-btn">수정</button>
-          <button className="clear-btn">삭제</button>
-        </div>
-      </li>
-    )
-  }
+const ExpenseItem = ({ expense, handleDelete, handleEdit }) => {
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expense">{expense.charge}</span>
+        <span className="amount">{expense.amount}원</span>
+      </div>
+      <div>
+        <button className="edit-btn" onClick={() => handleEdit(expense.id)}>
+          <MdEdit />
+        </button>
+        <button className="clear-btn" onClick={() => handleDelete(expense.id)}>
+          <MdDelete />
+        </button>
+      </div>
+    </li>
+  )
 }
 
 export default ExpenseItem
